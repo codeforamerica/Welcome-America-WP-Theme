1422e1cf530a0f3ba66afc44d49f143dd3a5caa1
John Mertens <john.mertens@gmail.com>
2011-03-20 18:32:00 -0700

    Change structure of page.php to match/mimic old site structure.

--- page.php
+++ page.php
@@ -1,14 +1,27 @@
 <?php get_header(); ?>
-
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div id="contentwrap">
+	<div id="content">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div class="leftcol">
 			
-		<div class="post" id="post-<?php the_ID(); ?>">
-
-			<h2><?php the_title(); ?></h2>
-
-			<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+			
+			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Left Sidebar') ) : ?>
+			<?php endif; ?>
+			
+			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Right Sidebar') ) : ?>
+			<?php endif; ?>
+		</div>
+		
+		<div class="rightcol">
+			<div class="photoholder"><?php echo get_the_post_thumbnail($post->ID); ?><img width="152" height="214" src="images/pgImages/wawa-hoagie-day.jpg"></div>
+		</div>
+		
+			
+		<div class="post textcolumn" id="post-<?php the_ID(); ?>">
 
 			<div class="entry">
+				
+				<h1><?php the_title(); ?></h1>
 
 				<?php the_content(); ?>
 
@@ -23,7 +36,8 @@
 		<?php comments_template(); ?>
 
 		<?php endwhile; endif; ?>
-
-<?php get_sidebar(); ?>
+	</div>
+</div>
+<?php //get_sidebar(); ?>
 
 <?php get_footer(); ?>
