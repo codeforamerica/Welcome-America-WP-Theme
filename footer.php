7737cce84be8d86079b6119629212b1f0bf3ed02
John Mertens <john.mertens@gmail.com>
2011-03-25 10:01:49 -0700

    Add widget areas to the footer for the sponsors and footer menu.

--- footer.php
+++ footer.php
@@ -1,34 +1,15 @@
 		<div id="footerwrap">
-		  <div id="footer">
-		  <div style="width: 960px; margin-left: auto; margin-right: auto; background-color: rgb(255, 255, 255); height: 105px;">
-
-		     <ul class="homelogos1 top">
-		      <li><a target="_blank" href="http://www.wawa.com"><img width="61" height="51" alt="" src="images/footerlogos/wawa.gif"></a></li>
-		      <li><a target="_blank" href="http://abclocal.go.com/wpvi/index"><img width="40" height="51" alt="" src="images/footerlogos/6abc.gif"></a></li>
-		      <li><a target="_blank" href="http://www.peco.com"><img width="94" height="51" alt="" src="images/footerlogos/peco.gif"></a></li>
-		      <li><a target="_blank" href="http://www.phl.org"><img width="99" height="51" alt="" src="images/footerlogos/phl.gif"></a></li>
-		     </ul>
-		     <ul class="homelogos1 bottom">
-		      <li><a target="_blank" href="http://www.phila.gov"><img width="93" height="51" alt="" src="images/footerlogos/city-of-philadelphia.gif"></a></li>
-		      <li><a target="_blank" href="http://www.fairmountpark.org/"><img width="44" height="51" alt="" src="images/footerlogos/fairmont.jpg"></a></li>
-		      <li><a target="_blank" href="http://www.state.pa.us"><img width="90" height="51" alt="" src="images/footerlogos/pa.gif"></a></li>
-		    </ul>
-
-		   </div>
-
-		</div>
-
-		<ul class="footernav">
-			<li class="n1 first"><a target="_self" href="about-us">About Us</a></li>
-			<li class="n2"><a target="_self" href="message-from-the-mayor">Message from the Mayor</a></li>
-			<li class="n3"><a target="_self" href="pressroom">Pressroom</a></li>
-			<li class="n4"><a target="_self" href="announcement">Announcements</a></li>
-			<li class="n5 last"><a target="_self" href="local-attractions">Local Attractions</a></li>
-		</ul>
+			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer menu') ) ?>
 		    <div class="copyright">
-		    &copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>. All Rights Reserved. &nbsp;&nbsp;&nbsp;&nbsp;Designed by <a href="http://www.osiris-inc.com">Osiris Group, Inc</a>	</div>
+		    &copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>. All Rights Reserved. &nbsp;&nbsp;&nbsp;&nbsp;Designed by <a href="http://www.osiris-inc.com">Osiris Group, Inc</a>	
 			</div>
-
+			<div id="sponsor-wrapper">
+				<div id="footer">
+					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sponsors') )  ?>
+					<br style="clear:both" />
+				</div>
+			</div>
+		</div>
 	<?php wp_footer(); ?>
 	
 	<!-- Don't forget analytics -->
