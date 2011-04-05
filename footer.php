d9c165c8d7aacc40a6d9b5d4cbba5cc3105c624b
John Mertens <john.mertens@gmail.com>
2011-04-05 10:19:56 -0700

    Restructure the footer so the links are inside the content area

--- footer.php
+++ footer.php
@@ -1,13 +1,14 @@
 		<div id="footerwrap">
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer menu') ) ?>
-		    <div class="copyright">
-		    &copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>. All Rights Reserved. &nbsp;&nbsp;&nbsp;&nbsp;Designed by <a href="http://www.osiris-inc.com">Osiris Group, Inc</a>	
+			<div id="footer">
+				<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer menu') ) ?>
+				<br />
+			    <div class="copyright">
+			    	&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>. All Rights Reserved. &nbsp;&nbsp;&nbsp;&nbsp;Designed by <a href="http://www.osiris-inc.com">Osiris Group, Inc</a>	
+				</div>
 			</div>
 			<div id="sponsor-wrapper">
-				<div id="footer">
-					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sponsors') )  ?>
-					<br style="clear:both" />
-				</div>
+				<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Sponsors') )  ?>
+				<br style="clear:both" />
 			</div>
 		</div>
 	<?php wp_footer(); ?>
