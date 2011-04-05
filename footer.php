f174b25e9e7a2fe031e2ba604484103efc4a56bf
Pete Fecteau <pete@buttonpresser.com>
2011-04-05 10:13:01 -0700

    Added Code for America link to footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		<div id="footerwrap">
 			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer menu') ) ?>
 		    <div class="copyright">
-		    &copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>. All Rights Reserved. &nbsp;&nbsp;&nbsp;&nbsp;Designed by <a href="http://www.osiris-inc.com">Osiris Group, Inc</a>	
+		    &copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>. All Rights Reserved. &nbsp;&nbsp;&nbsp;&nbsp;Designed by <a href="http://www.osiris-inc.com">Osiris Group, Inc</a>.  &nbsp;&nbsp;&nbsp;&nbsp;Developed by <a href="http://codeforamerica.org/" title="Code for America">Code for America</a>	
 			</div>
 			<div id="sponsor-wrapper">
 				<div id="footer">
